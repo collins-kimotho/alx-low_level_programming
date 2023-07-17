eea9d860868c1f1f2f1b10a494d58da0cb174514
collins-kimotho <collinskimotho16@gmail.com>
2023-07-17 13:54:28 -0700

    puts_function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 /**
- * main Using the puts functioni
- * Return: 0 (success)
- */ 
-int main (void)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	put("\"Programming is like building a multilingual puzzle");
+	puts("/"Programming is like building a multilingual puzzle");
 	return (0);
 }
