9ff84ec9a2251f497f05d4ce1fa95bae992fb2e1
collins-kimotho <collinskimotho16@gmail.com>
2023-07-17 15:19:29 -0700

    size-of-dtypes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,16 +6,10 @@
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
-
-	printf("Size of char: %lu bytes(s)\n", (unsigned long)sizeof(a));
-	printf("Size of an int: %lu bytes(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
-	printf("Size of a float: %lu byes(s)\n", sizeof(e));
+	printf("Size of a char: %2d byte(s) \n", sizeof(char));
+	printf("Size of an int: %2d byte(s) \n", sizeof(int));
+	printf("Size of a long int: %2d byte(s) \n", sizeof(long int));
+	printf("Sizze of a long long int: %2d byte(s) \n", sizeof(long double));
+	printf("Size of a float: %2d byte(s) \n", sizeof(float));
 	return (0);
 }
