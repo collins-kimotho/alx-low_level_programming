815de1826c4f47252b58efa5fb30e9ca9bb2b488
collins-kimotho <collinskimotho16@gmail.com>
2023-07-18 10:09:13 -0700

    single_digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * Entry - main
+ *
+ * Describe: Print all single digit numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
