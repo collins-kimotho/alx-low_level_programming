be5785858c72a0e242b628888bbb3383af89de51
collins-kimotho <collinskimotho16@gmail.com>
2023-07-25 13:09:30 -0700

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
- * main - swap the values of two integers
- *
- * Return: Awalys 0
+ * reset_to_98 - function to update a parameter
+ * @n: input
+ * Return: n
  */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
