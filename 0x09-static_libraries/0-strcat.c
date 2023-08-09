f7c399f58b0112b8ebedd5cbbfb085f5dea0880b
collins-kimotho <collinskimotho16@gmail.com>
2023-08-08 18:09:46 -0700

    putchar

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,30 +1,24 @@
 #include "main.h"
- /**
- * _strcat - concatenates two strings
- * @dest: input value
- * @src: input value
- *
- * Return: void	
+
+/**
+ * _strcat - concatenate two strings
+ * @dest: char string to concatenate to
+ * @src: char string
+ * Return: pointer to resulting string `dest`
  */
+
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int i, c;
 
+	for (i = 0; dest[i] != '\0'; i++)
+		;
 
-	i = 0;
-	while (dest[i] != '\0')
+	for (c = 0; src[c] != '\0'; c++)
 	{
+		dest[i] = src[c];
 		i++;
 	}
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}		
-
 
 	dest[i] = '\0';
 	return (dest);
