1c9f7f0ce718fd4b245c13c81b31c058e82f5164
collins-kimotho <collinskimotho16@gmail.com>
2023-08-28 10:53:34 -0700

    number-of-elements

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -17,6 +17,9 @@ typedef struct listint_s
 
 /* Provided putchar function */
 int _putchar(char c);
+
+/* Function prototypes*/
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 
 #endif /* LISTS_H*/
