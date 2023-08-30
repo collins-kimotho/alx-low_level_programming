ce38b6f1d6c8788b15a7ab6ab99963bd6dc146b4
collins-kimotho <collinskimotho16@gmail.com>
2023-08-30 15:21:08 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
-#include<stdio.h>
-#include<unistd.h>
+#include <unistd.h>
+
 /**
- * main - Entry point
+ * main - Entry point of the program
  *
- * Return: Always 0 (Success)
+ * Description: Prints a specific messsage to the standard error stream.
+ *
+ * Return: Always 1 (indicating an error)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19));
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
