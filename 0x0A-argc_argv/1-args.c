029dcde6849700188b5ce4db04cb1fa8bc1c62f4
collins-kimotho <collinskimotho16@gmail.com>
2023-10-02 03:48:57 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: The number of command line arguments
+ * @argv: The command line arguments
+ *
+ * Return: 0 if successful
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", arge - 1);
+	return (0);
+}
