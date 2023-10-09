ddc8b2e1e5a99c602c515f9341df6139c76703bc
collins-kimotho <collinskimotho16@gmail.com>
2023-10-09 16:20:23 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - Information about a dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * Description: A new type struct that containes information about a dog
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner
+} my_dog;
