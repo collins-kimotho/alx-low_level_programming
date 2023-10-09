79440ca4a8f549ed2515c7ff2181d2445034a2ba
collins-kimotho <collinskimotho16@gmail.com>
2023-10-09 16:26:23 -0700

    struct-dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
  * struct dog - Information about a dog
  * @name: The name of the dog
@@ -12,3 +15,5 @@ typedef struct dog
 	float age;
 	char *owner;
 } my_dog;
+
+#endif /* DOG_H */
