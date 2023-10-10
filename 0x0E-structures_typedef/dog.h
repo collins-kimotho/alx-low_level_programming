f0b31524ac8da0080450cfa094c5677d76b8dec8
collins-kimotho <collinskimotho16@gmail.com>
2023-10-10 02:41:26 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,4 +18,12 @@ typedef struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+/**
+ * dog_t - Typedef for struct dog
+ *
+ * Description: Defines a new type dog_t as a new name for the type struct dog
+ */
+typedef struct dog dog_t;
+
 #endif /* DOG_H */
