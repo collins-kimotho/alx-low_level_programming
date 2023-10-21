daced5d595fabaf8841c919e8fc34e2968c8b85f
collins-kimotho <collinskimotho16@gmail.com>
2023-10-21 12:25:35 -0700

    print-list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -18,7 +18,7 @@ size_t print_list(const list_t *h)
 		if (h->str)
 			printf("[%d] %s\n", h->len, h->str);
 		else
-			printf("[0] (nill)\n");
+			printf("[0] (nil)\n");
 		k++;
 		h = h->next;
 	}
