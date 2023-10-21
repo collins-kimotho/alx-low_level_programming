edcfa8118651ef51f744d8dc71d719539c7bfcef
collins-kimotho <collinskimotho16@gmail.com>
2023-10-21 12:58:26 -0700

    add-node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,9 +11,14 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_node;
+	unsigned int len = 0;
 
-	if(!new_node)
+	while (str[len])
+		len++;
+
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
 		return (NULL);
 
 	new_node->str = strdup(str);
