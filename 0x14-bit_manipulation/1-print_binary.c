533abf6e6a7fcb573eef17056a83001388bc506b
collins-kimotho <collinskimotho16@gmail.com>
2023-10-26 16:54:33 -0700

    print-binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -18,10 +18,10 @@ void print_binary(unsigned long int n)
 	}
 
 	/* iterate over the input */
-	  mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
-	  while (mask)
-	  {
-		  _putchar (((n & mask) != 0) ? '1' : '0');
-		  mask >>= 1;
-	  }
+	mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	while (mask)
+	{
+		_putchar (((n & mask) != 0) ? '1' : '0');
+		mask >>= 1;
+	}
 }
