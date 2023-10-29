31adf7952ed991a5fb9de2b22c174290b1ca706f
collins-kimotho <collinskimotho16@gmail.com>
2023-10-29 05:55:20 -0700

    puts-recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: pointer to the string
+ *
+ * Description: this is a recursive function
+ */
+void _puts_recursion(char *s)
+{
+	/*base case*/
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
