8330d51dad5be8fba09a493b1a028183ffe8b8e8
collins-kimotho <collinskimotho16@gmail.com>
2023-12-18 13:47:14 +0000

    addnode

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -25,7 +25,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	current = *h;
 	while (current != NULL)
 	{
-		if (count == idx -1)
+		if (count == idx - 1)
 		{
 			new_node = malloc(sizeof(dlistint_t));
 			if (new_node == NULL)
@@ -39,7 +39,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			{
 				current->next->prev = new_node;
 			}
-			current->next =new_node;
+			current->next = new_node;
 			return (new_node);
 		}
 		count++;
